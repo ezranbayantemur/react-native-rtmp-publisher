a8de4910b207e420d48ddff96a8d47e21ce31f3c
Ezran <ezranbayantemur@gmail.com>
2022-01-07 03:13:33 +0300

    fix: fix-android-import
    Fixed renamed package import

--- example/android/app/src/main/java/com/example/reactnativertmp/MainApplication.java
+++ example/android/app/src/main/java/com/example/reactnativertmp/MainApplication.java
@@ -10,7 +10,7 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.reactnativertmp.RTMPPackage;
+import com.reactnativertmppublisher.RTMPPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
