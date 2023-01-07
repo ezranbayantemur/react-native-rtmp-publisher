196e9de0cd5f8a8868f6cfed6b7e0e1ef8fcf09e
Ezran Bayantemur <ezranbayantemur@gmail.com>
2023-01-08 01:54:53 +0300

    Fixed bug on example

--- example/src/hooks/usePermissions.ts
+++ example/src/hooks/usePermissions.ts
@@ -2,7 +2,8 @@ import { useState, useEffect } from 'react';
 import { PermissionsAndroid, Platform } from 'react-native';
 
 const CAMERA_PERMISSION = PermissionsAndroid.PERMISSIONS.CAMERA;
-const AUDIO_PERMISSION = PermissionsAndroid.PERMISSIONS.CAMERA;
+const AUDIO_PERMISSION = PermissionsAndroid.PERMISSIONS.RECORD_AUDIO;
+const BLUETOOTH_PERMISSION = PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT;
 
 function usePermissions() {
   const [permissionGranted, setPermissionGranted] = useState<boolean>(false);
@@ -19,14 +20,18 @@ function usePermissions() {
 
     const cameraPermission = await PermissionsAndroid.check(CAMERA_PERMISSION);
     const audioPermission = await PermissionsAndroid.check(AUDIO_PERMISSION);
+    const bluetoothPermission = await PermissionsAndroid.check(
+      BLUETOOTH_PERMISSION
+    );
 
-    if (cameraPermission && audioPermission) {
+    if (cameraPermission && audioPermission && bluetoothPermission) {
       return setPermissionGranted(true);
     }
 
     const hasPermissions = await PermissionsAndroid.requestMultiple([
       CAMERA_PERMISSION,
       AUDIO_PERMISSION,
+      BLUETOOTH_PERMISSION,
     ]);
 
     if (hasPermissions) {
